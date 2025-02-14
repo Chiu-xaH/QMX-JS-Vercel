48c2c1a32a7009106e893eea377dcbee830a78d3
Chiu <zsh0908@outlook.com>
2025-02-14 23:37:45 +0800

    1

---
+++ api/main.js
@@ -0,0 +1,357 @@
+// 补环境
+ 
+
+var main
+require('./vector');
+!function(e) {
+    function t(t) {
+        for (var r, n, d = t[0], c = t[1], i = t[2], l = 0, b = []; l < d.length; l++)
+            n = d[l],
+            Object.prototype.hasOwnProperty.call(o, n) && o[n] && b.push(o[n][0]),
+            o[n] = 0;
+        for (r in c)
+            Object.prototype.hasOwnProperty.call(c, r) && (e[r] = c[r]);
+        for (u && u(t); b.length; )
+            b.shift()();
+        return f.push.apply(f, i || []),
+        a()
+    }
+    function a() {
+        for (var e, t = 0; t < f.length; t++) {
+            for (var a = f[t], r = !0, n = 1; n < a.length; n++) {
+                var c = a[n];
+                0 !== o[c] && (r = !1)
+            }
+            r && (f.splice(t--, 1),
+            e = d(d.s = a[0]))
+        }
+        return e
+    }
+    var r = {}
+      , n = {
+        21: 0
+    }
+      , o = {
+        21: 0
+    }
+      , f = [];
+    function d(t) {
+        if (r[t])
+            return r[t].exports;
+        var a = r[t] = {
+            i: t,
+            l: !1,
+            exports: {}
+        };
+        return e[t].call(a.exports, a, a.exports, d),
+        a.l = !0,
+        a.exports
+    }
+    d.e = function(e) {
+        var t = [];
+        n[e] ? t.push(n[e]) : 0 !== n[e] && {
+            1: 1,
+            3: 1,
+            4: 1,
+            5: 1,
+            6: 1,
+            7: 1,
+            8: 1,
+            9: 1,
+            10: 1,
+            11: 1,
+            12: 1,
+            13: 1,
+            14: 1,
+            15: 1,
+            16: 1,
+            17: 1,
+            18: 1,
+            19: 1,
+            20: 1,
+            22: 1,
+            23: 1,
+            24: 1,
+            25: 1,
+            26: 1
+        }[e] && t.push(n[e] = new Promise((function(t, a) {
+            for (var r = "css/" + ({
+                1: "common",
+                3: "album",
+                4: "albumDetail",
+                5: "album_mall",
+                6: "category",
+                7: "cmtpage",
+                8: "download_detail",
+                9: "index",
+                10: "msg_center",
+                11: "mv",
+                12: "mvList",
+                13: "mv_toplist",
+                14: "notfound",
+                15: "player",
+                16: "player_radio",
+                17: "playlist",
+                18: "playlist_edit",
+                19: "profile",
+                20: "radio",
+                22: "search",
+                23: "singer",
+                24: "singer_list",
+                25: "songDetail",
+                26: "toplist"
+            }[e] || e) + "." + {
+                1: "092d215c4a601df90f9f",
+                3: "5cf0d69eaf29bcab23d2",
+                4: "798353db5b0eb05d5358",
+                5: "df4c243f917604263e58",
+                6: "20d532d798099a44bc88",
+                7: "e3bedf2b5810f8db0684",
+                8: "e3bedf2b5810f8db0684",
+                9: "ea0adb959fef9011fc25",
+                10: "020422608fe8bfb1719a",
+                11: "8bdb1df6c5436b790baa",
+                12: "47ce9300786df1b70584",
+                13: "4aee33230ba2d6b81dce",
+                14: "e6f63b0cf57dd029fbd6",
+                15: "1d2dbefbea113438324a",
+                16: "d893492de07ce97d8048",
+                17: "9484fde660fe93d9f9f0",
+                18: "67fb85e7f96455763c83",
+                19: "5e8c651e74b13244f7cf",
+                20: "3befd83c10b19893ec66",
+                22: "b2d11f89ea6a512a2302",
+                23: "c7a38353c5f4ebb47491",
+                24: "df0961952a2d3f022894",
+                25: "4c080567e394fd45608b",
+                26: "8edb142553f97482e00f"
+            }[e] + ".chunk.css?max_age=2592000", o = d.p + r, f = document.getElementsByTagName("link"), c = 0; c < f.length; c++) {
+                var i = (u = f[c]).getAttribute("data-href") || u.getAttribute("href");
+                if ("stylesheet" === u.rel && (i === r || i === o))
+                    return t()
+            }
+            var l = document.getElementsByTagName("style");
+            for (c = 0; c < l.length; c++) {
+                var u;
+                if ((i = (u = l[c]).getAttribute("data-href")) === r || i === o)
+                    return t()
+            }
+            var b = document.createElement("link");
+            b.rel = "stylesheet",
+            b.type = "text/css",
+            b.onload = t,
+            b.onerror = function(t) {
+                var r = t && t.target && t.target.src || o
+                  , f = new Error("Loading CSS chunk " + e + " failed.\n(" + r + ")");
+                f.code = "CSS_CHUNK_LOAD_FAILED",
+                f.request = r,
+                delete n[e],
+                b.parentNode.removeChild(b),
+                a(f)
+            }
+            ,
+            b.href = o,
+            0 !== b.href.indexOf(window.location.origin + "/") && (b.crossOrigin = "anonymous"),
+            document.getElementsByTagName("head")[0].appendChild(b)
+        }
+        )).then((function() {
+            n[e] = 0
+        }
+        )));
+        var a = o[e];
+        if (0 !== a)
+            if (a)
+                t.push(a[2]);
+            else {
+                var r = new Promise((function(t, r) {
+                    a = o[e] = [t, r]
+                }
+                ));
+                t.push(a[2] = r);
+                var f, c = document.createElement("script");
+                c.charset = "utf-8",
+                c.timeout = 120,
+                d.nc && c.setAttribute("nonce", d.nc),
+                c.src = function(e) {
+                    return d.p + "js/" + ({
+                        1: "common",
+                        3: "album",
+                        4: "albumDetail",
+                        5: "album_mall",
+                        6: "category",
+                        7: "cmtpage",
+                        8: "download_detail",
+                        9: "index",
+                        10: "msg_center",
+                        11: "mv",
+                        12: "mvList",
+                        13: "mv_toplist",
+                        14: "notfound",
+                        15: "player",
+                        16: "player_radio",
+                        17: "playlist",
+                        18: "playlist_edit",
+                        19: "profile",
+                        20: "radio",
+                        22: "search",
+                        23: "singer",
+                        24: "singer_list",
+                        25: "songDetail",
+                        26: "toplist"
+                    }[e] || e) + ".chunk." + {
+                        1: "0371cfbdfdfbb3ad8cd5",
+                        3: "3bdf0d02438a736b0a00",
+                        4: "1fb54312478c8d8e0c7e",
+                        5: "cf3511dcc388397b68a4",
+                        6: "af732551fe2b645e9639",
+                        7: "674c0ae2f4a2766938d2",
+                        8: "66a969743fcc550e1f16",
+                        9: "6f20a9e999a46036af70",
+                        10: "a38d0d3603976bae1845",
+                        11: "1fc8b1197fd484665e2b",
+                        12: "70919edcab2aa8ee4169",
+                        13: "96f2d1c236f6b514c895",
+                        14: "63f10c7dbb60a5d7af1d",
+                        15: "74650bb0820de540d764",
+                        16: "c17f578eaa1aa117608a",
+                        17: "ede15da9f4f619db6807",
+                        18: "f78c184154c43c00ee40",
+                        19: "22928eb0d8a62710211b",
+                        20: "7a00d94da512ebedee19",
+                        22: "90df8b99f63fa17b8e59",
+                        23: "6644fac8fb724682de23",
+                        24: "cb6322b166d848e5a295",
+                        25: "93ce11f50772f76f2e72",
+                        26: "3cbf7a85a506c8004ece"
+                    }[e] + ".js?max_age=2592000"
+                }(e),
+                0 !== c.src.indexOf(window.location.origin + "/") && (c.crossOrigin = "anonymous");
+                var i = new Error;
+                f = function(t) {
+                    c.onerror = c.onload = null,
+                    clearTimeout(l);
+                    var a = o[e];
+                    if (0 !== a) {
+                        if (a) {
+                            var r = t && ("load" === t.type ? "missing" : t.type)
+                              , n = t && t.target && t.target.src;
+                            i.message = "Loading chunk " + e + " failed.\n(" + r + ": " + n + ")",
+                            i.name = "ChunkLoadError",
+                            i.type = r,
+                            i.request = n,
+                            a[1](i)
+                        }
+                        o[e] = void 0
+                    }
+                }
+                ;
+                var l = setTimeout((function() {
+                    f({
+                        type: "timeout",
+                        target: c
+                    })
+                }
+                ), 12e4);
+                c.onerror = c.onload = f,
+                document.head.appendChild(c)
+            }
+        return Promise.all(t)
+    }
+    ,
+    d.m = e,
+    d.c = r,
+    d.d = function(e, t, a) {
+        d.o(e, t) || Object.defineProperty(e, t, {
+            enumerable: !0,
+            get: a
+        })
+    }
+    ,
+    d.r = function(e) {
+        "undefined" !== typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+            value: "Module"
+        }),
+        Object.defineProperty(e, "__esModule", {
+            value: !0
+        })
+    }
+    ,
+    d.t = function(e, t) {
+        if (1 & t && (e = d(e)),
+        8 & t)
+            return e;
+        if (4 & t && "object" === typeof e && e && e.__esModule)
+            return e;
+        var a = Object.create(null);
+        if (d.r(a),
+        Object.defineProperty(a, "default", {
+            enumerable: !0,
+            value: e
+        }),
+        2 & t && "string" != typeof e)
+            for (var r in e)
+                d.d(a, r, function(t) {
+                    return e[t]
+                }
+                .bind(null, r));
+        return a
+    }
+    ,
+    d.n = function(e) {
+        var t = e && e.__esModule ? function() {
+            return e.default
+        }
+        : function() {
+            return e
+        }
+        ;
+        return d.d(t, "a", t),
+        t
+    }
+    ,
+    d.o = function(e, t) {
+        return Object.prototype.hasOwnProperty.call(e, t)
+    }
+    ,
+    d.p = "/ryqq/",
+    d.oe = function(e) {
+        throw e
+    }
+    ;
+    var c = window.webpackJsonp = window.webpackJsonp || []
+      , i = c.push.bind(c);
+    c.push = t,
+    c = c.slice();
+    for (var l = 0; l < c.length; l++)
+        t(c[l]);
+    var u = i;
+    a()
+    main = d
+}([]);
+
+
+
+
+function get_sign(data) {
+    o = main(350).default
+    sign = o(data)
+    return sign
+}
+
+module.exports = { get_sign };
+
+module.exports = (req, res) => {
+  const { data } = req.query; // 从请求中获取传递的 `data`
+
+  if (!data) {
+    return res.status(400).json({ error: 'Missing data parameter' });
+  }
+
+  try {
+    const sign = get_sign(data); // 调用 get_sign 函数
+    res.status(200).json({ sign });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
